c011a08fa8bb4ddca32c174a70f521e45ea9f957
Roman <roman.pawliw@gmail.com>
2017-10-26 20:55:40 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+var app = express();
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/test' || process.env.MONGODB_URI, { useMongoClient: true });
+
+var Client = mongoose.model('Client', {client: String,
+                                       version: String,
+                                       key: String,
+                                       value: String});
+
+app.get("/config/:client/:version", function(req, res) {
+  Client.find({client: req.params.client, version: req.params.version}, function(err, clients) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      var clientsConfig = {};
+
+      for (var i = 0; i < clients.length; i++) {
+        var key = clients[i].key;
+        clientsConfig[key] = clients[i].value;
+      }
+
+      res.jsonp(clientsConfig);
+    }
+  });
+});
+
+app.post("/config", function(req, res) {
+
+  var client = new Client(req.body);
+
+  client.save(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.status(201).send('Created');
+    }
+  });
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {client: '', version: '', key: '', value: ''}
+    this.createClient = this.createClient.bind(this);
+    this.clientData = this.clientData.bind(this);
+    this.getClient = this.getClient.bind(this);
+  }
+
+  clientData(event) {
+    const target = event.target;
+    const name = target.name;
+
+    this.setState({[name]: event.target.value});
+  }
+
+  createClient(event) {
+    axios.post('/config', {
+      client: this.state.client,
+      version: this.state.version,
+      key: this.state.key,
+      value: this.state.value
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+    event.preventDefault();
+  }
+
+  getClient(event) {
+    axios.get('/config/' + this.state.client + '/' + this.state.version, {
+    })
+    .then(function (response) {
+      console.log('response***', response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Client App management</h1>
+        </header>
+
+        <p className="App-intro">
+          <form onSubmit={this.createClient}>
+            <p>
+              <label>
+                Identifier for the client. E.g. "ios":
+                <input type="text" name="client" onChange={this.clientData} required />
+              </label>
+            </p>
+            <p>
+              <label>
+                Build version of the client. E.g. "267":
+                <input type="text" name="version" onChange={this.clientData} required />
+              </label>
+            </p>
+            <p>
+              <label>
+                Key:
+                <input type="text" name="key" onChange={this.clientData} required />
+              </label>
+            </p>
+            <p>
+              <label>
+                Value:
+                <input type="text" name="value" onChange={this.clientData} required />
+              </label>
+            </p>
+            <input type="submit" value="Submit" />
+          </form>
+
+          <form onSubmit={this.getClient}>
+            <label>
+              Client:
+              <input type="text" name="client" onChange={this.clientData} required />
+            </label>
+            <label>
+              Version:
+              <input type="text" name="version" onChange={this.clientData} required />
+            </label>
+
+            <input type="submit" value="Get Client" />
+          </form>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
